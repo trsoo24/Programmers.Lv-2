df9e993fdc419b56d8c31528d6dfe8c25d0d8b36
trsoo24 <122004333+trsoo24@users.noreply.github.com>
2023-02-10 00:01:16 +0900

    Update 주식가격.java

--- 주식가격.java
+++ 주식가격.java
@@ -15,3 +15,31 @@ class Solution {
         return answer;
     }
 }
+
+
+// 다른 사람의 스택 풀이
+
+// import java.util.Stack;
+
+// class Solution {
+//     public int[] solution(int[] prices) {
+//         Stack<Integer> beginIdxs = new Stack<>();
+//         int i=0;
+//         int[] terms = new int[prices.length];
+
+//         beginIdxs.push(i);
+//         for (i=1; i<prices.length; i++) {
+//             while (!beginIdxs.empty() && prices[i] < prices[beginIdxs.peek()]) {
+//                 int beginIdx = beginIdxs.pop();
+//                 terms[beginIdx] = i - beginIdx;
+//             }
+//             beginIdxs.push(i);
+//         }
+//         while (!beginIdxs.empty()) {
+//             int beginIdx = beginIdxs.pop();
+//             terms[beginIdx] = i - beginIdx - 1;
+//         }
+
+//         return terms;
+//     }
+// }
