224db466bee4c4e4d8687223c54ffbc04880bbb9
trsoo24 <122004333+trsoo24@users.noreply.github.com>
2023-02-11 12:10:38 +0900

    Create 올바른 괄호.java

---
+++ 올바른 괄호.java
@@ -0,0 +1,40 @@
+// 괄호가 바르게 짝지어졌다는 것은 '(' 문자로 열렸으면 반드시 짝지어서 ')' 문자로 닫혀야 한다는 뜻입니다. 
+// 예를 들어 "()()" 또는 "(())()" 는 올바른 괄호입니다.
+//          ")()(" 또는 "(()(" 는 올바르지 않은 괄호입니다.
+//          '(' 또는 ')' 로만 이루어진 문자열 s가 주어졌을 때, 문자열 s가 올바른 괄호이면 true를 return 하고, 
+//          올바르지 않은 괄호이면 false를 return 하는 solution 함수를 완성해 주세요.
+
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        if(s.length() % 2 != 0 || s.charAt(0) == '(' 
+           || s.charAt(s.length() -1) == ')') {
+            answer = false;
+        }
+        
+        stack.push(s.charAt(0));
+        
+        for(int i = 1; i < s.length(); i++) {
+            if(s.charAt(i) == '(') {
+                stack.push(s.charAt(i));
+            } else if (s.charAt(i) == ')') {
+                    if (stack.isEmpty()) {
+                        return false;
+                    } else {
+                        stack.pop();
+                    }
+                }
+            }
+        
+        if(stack.isEmpty()){
+            answer = true;
+        } else {
+            answer = false;
+        }
+        return answer;
+    }
+}
