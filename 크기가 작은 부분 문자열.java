4c8285f99d457b669e6471e3e4f797b58c0616b1
trsoo24 <122004333+trsoo24@users.noreply.github.com>
2023-03-22 13:59:30 +0900

    Create 크기가 작은 부분 문자열.java

---
+++ 크기가 작은 부분 문자열.java
@@ -0,0 +1,25 @@
+// 숫자로 이루어진 문자열 t와 p가 주어질 때, t에서 p와 길이가 같은 부분문자열 중에서,
+// 이 부분문자열이 나타내는 수가 p가 나타내는 수보다 작거나 같은 것이 나오는 횟수를 return하는 함수 solution을 완성하세요.
+
+// 예를 들어, t="3141592"이고 p="271" 인 경우, t의 길이가 3인 부분 문자열은 314, 141, 415, 159, 592입니다. 
+//  이 문자열이 나타내는 수 중 271보다 작거나 같은 수는 141, 159 2개 입니다.
+
+class Solution {
+    // 숫자로 이루어진 문자열 t, p
+    // t 에서 p와 길이가 같은 부분 문자열
+    // 이 부분문자열이 나타내는 수가 p 가 나타내는 수보다 작거나 같은 것의 갯수 리턴
+
+    public static int solution(String t, String p) {
+        int len = p.length();
+        long num = Long.parseLong(p);
+        int answer = 0;
+
+        for (int i = 0; i <= t.length() - len; i++) {
+            if (Long.parseLong(t.substring(i, i + len)) <= num) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
